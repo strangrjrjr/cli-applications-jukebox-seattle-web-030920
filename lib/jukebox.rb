f948ccd0540164b78692c8692f5af5692b665782
Roger Robertson Jr <rjrjr@pm.me>
2020-02-07 16:33:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
-  puts choice
+  #puts choice
   found = false
   songs.each { |song|
     if choice.to_i == (song.index - 1) || choice == song
